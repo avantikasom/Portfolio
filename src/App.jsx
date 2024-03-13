4b4b0a97044ffa4e7671c7df9b363703cba41e15
avantika <avantikasom977@gmail.com>
2024-03-13 20:44:26 +0530

    feature(Footer.jsx): add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
